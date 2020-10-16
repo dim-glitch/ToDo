ac19f28037316848a2f1735e9c1720783554464d
dim-glitch <qwerty9009zxc@gmail.com>
2020-10-16 13:21:58 +0300

    spread

--- src/index.js
+++ src/index.js
@@ -7,11 +7,16 @@ import TodoList from './components/todo-list';
 
 const App = () =>{
  
+  const todoData = [
+    {label: 'Drink Coffee', important: false},
+    {label: 'Make Awesome App', important: true},
+    {label: 'Have a Lunch', important: false}
+  ];
   return(
     <div>
       <AppHeader />
       <SearchPanel />
-      <TodoList />
+      <TodoList todos = { todoData } />
     </div>
   );
 };
